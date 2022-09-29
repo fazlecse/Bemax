7a8148824915c5b2f2b28d9e15bfea52ac100783
fazlecse <mdfazlerabbi06@gmail.com>
2022-09-29 17:42:44 +0600

    project area

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,6 +24,26 @@
     }
 })
 
+// project_carousel_area
+$('.project_carousel_area').owlCarousel({
+    loop:true,
+    autoplay:true,
+    autoplayTimeout: 3000,
+    nav: true,
+    navText:['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
 
 
 
