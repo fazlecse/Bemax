cdc214529963118eb97156e10d0464fff8c7096e
fazlecse <mdfazlerabbi06@gmail.com>
2022-10-01 20:19:46 +0600

    home page done

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,14 +45,8 @@ $('.project_carousel_area').owlCarousel({
     }
 })
 
-// magnific_popup
-$('.popup_video').magnificPopup({
-    type: 'image'
-    // other options
-  })
-
 //   blog_carousel
-$('.blog_carousel_left').owlCarousel({
+$('.blog_carousel_angle').owlCarousel({
     loop:true,
     autoplay:true,
     dots:false,
@@ -72,6 +66,37 @@ $('.blog_carousel_left').owlCarousel({
     }
 })
 
+
+//   brand_carousel
+$('.brand_carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    dots:false,
+    autoplayTimeout: 4000,
+    margin:20,
+    nav: true,
+    navText:['<i class="fa-solid fa-angle-left"></i>','<i class="fa-solid fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+
+
+
+// magnific_popup
+$('.popup_video').magnificPopup({
+    type: 'image'
+    // other options
+  })
 
 
 })(jQuery)
